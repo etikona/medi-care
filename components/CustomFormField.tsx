791359bec6c9026fc62e94e316c3213cae8062e3
eti kona <etikonapal@gmail.com>
2024-07-17 11:04:22 +0600

    form structure is done

--- components/CustomFormField.tsx
+++ components/CustomFormField.tsx
@@ -10,6 +10,9 @@ import {
 import { Control } from "react-hook-form";
 import { Input } from "@/components/ui/input";
 import { FormFieldType } from "./forms/PatientForm";
+import Image from "next/image";
+import "react-phone-number-input/style.css";
+import PhoneInput from "react-phone-number-input";
 
 interface CustomProps {
   control: Control<any>;
@@ -26,10 +29,43 @@ interface CustomProps {
   renderSkeleton?: (field: any) => React.ReactNode;
 }
 const RenderField = ({ field, props }: { field: any; props: CustomProps }) => {
+  const { fieldType, iconSrc, iconAlt, placeholder } = props;
   switch (props.fieldType) {
     case FormFieldType.INPUT:
       return (
-        <div className="flex rounded-md border-dark-500 bg-dark-400"></div>
+        <div className="flex rounded-md border-dark-500 bg-dark-400">
+          {iconSrc && (
+            <Image
+              src={iconSrc}
+              alt={iconAlt || "icon"}
+              height={24}
+              width={24}
+              className="ml-2"
+            />
+          )}
+
+          <FormControl>
+            <Input
+              placeholder={placeholder}
+              {...field}
+              className="shad-input border-0"
+            />
+          </FormControl>
+        </div>
+      );
+    case FormFieldType.PHONE_INPUT:
+      return (
+        <FormControl>
+          <PhoneInput
+            defaultCountry="US"
+            placeholder={placeholder}
+            international
+            withCountryCode
+            value={field.value as number | undefined}
+            onChange={field.onChange}
+            className="input-phone"
+          />
+        </FormControl>
       );
     default:
       break;
--- components/forms/PatientForm.tsx
+++ components/forms/PatientForm.tsx
@@ -6,7 +6,12 @@ import { z } from "zod";
 import { Button } from "@/components/ui/button";
 import { Form } from "@/components/ui/form";
 import icon from "@/public/assets/icons/user.svg";
-import CustomeFormField from "../CustomeFormField";
+import email from "@/public/assets/icons/email.svg";
+// import phone from "@/public/assets/icons/phone.svg";
+import CustomFormField from "../CustomFormField";
+import SubmitButton from "../SubmitButton";
+import { useState } from "react";
+// import CustomeFormField from "../CustomeFormField";
 export enum FormFieldType {
   INPUT = "input",
   TEXTAREA = "textarea",
@@ -23,6 +28,7 @@ const formSchema = z.object({
 });
 
 const PatientForm = () => {
+  const [isLoading, setIsLoading] = useState(false);
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
@@ -40,16 +46,32 @@ const PatientForm = () => {
           <h1 className="header">Hi there👋</h1>
           <p className="">Schedule your first appointment</p>
         </section>
-        <CustomeFormField
+        <CustomFormField
           fieldType={FormFieldType.INPUT}
           control={form.control}
           name="name"
-          label="full name"
+          label="Full Name"
           placeholder="John Doe"
           iconSrc={icon}
           iconAlt="User icon"
         />
-        <Button type="submit">Submit</Button>
+        <CustomFormField
+          fieldType={FormFieldType.INPUT}
+          control={form.control}
+          name="email"
+          label="Email"
+          placeholder="johndoe@gmail.com"
+          iconSrc={email}
+          iconAlt="Email icon"
+        />
+        <CustomFormField
+          fieldType={FormFieldType.PHONE_INPUT}
+          control={form.control}
+          name="Phone"
+          label="Phone Number"
+          placeholder="+8801289345"
+        />
+        <SubmitButton isLoading={isLoading}> GET STARTED </SubmitButton>
       </form>
     </Form>
   );
