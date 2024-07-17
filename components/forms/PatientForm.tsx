cd53e9b93d6551ba27a56ebadabcd281b9dd9baf
eti kona <etikonapal@gmail.com>
2024-07-18 03:41:46 +0600

    form frontend completed

--- components/forms/PatientForm.tsx
+++ components/forms/PatientForm.tsx
@@ -11,6 +11,8 @@ import email from "@/public/assets/icons/email.svg";
 import CustomFormField from "../CustomFormField";
 import SubmitButton from "../SubmitButton";
 import { useState } from "react";
+import { UserFormValidation } from "@/lib/validation";
+import { useRouter } from "next/navigation";
 // import CustomeFormField from "../CustomeFormField";
 export enum FormFieldType {
   INPUT = "input",
@@ -21,23 +23,36 @@ export enum FormFieldType {
   SELECT = "select",
   SKELETON = "skeleton",
 }
-const formSchema = z.object({
-  username: z.string().min(2, {
-    message: "Username must be at least 2 characters.",
-  }),
-});
 
 const PatientForm = () => {
+  const router = useRouter();
   const [isLoading, setIsLoading] = useState(false);
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
+  const form = useForm<z.infer<typeof UserFormValidation>>({
+    resolver: zodResolver(UserFormValidation),
     defaultValues: {
-      username: "",
+      name: "",
+      email: "",
+      phone: "",
     },
   });
 
-  function onSubmit(values: z.infer<typeof formSchema>) {
-    console.log(values);
+  async function onSubmit({
+    name,
+    email,
+    phone,
+  }: z.infer<typeof UserFormValidation>) {
+    setIsLoading(true);
+    try {
+      // const userData = {
+      //   name,
+      //   email,
+      //   phone,
+      // };
+      // const user = await createUser(userDate);
+      // if(user) router.push(`/patients/${user.$id}/register`)
+    } catch (error) {
+      console.log(error);
+    }
   }
   return (
     <Form {...form}>
---
+++ lib/validation.ts
@@ -0,0 +1,11 @@
+import { z } from "zod";
+export const UserFormValidation = z.object({
+  name: z
+    .string()
+    .min(2, "Username must be at least 2 characters.")
+    .max(50, "Username less than 50 characters."),
+  email: z.string().email("Invalid email address"),
+  phone: z
+    .string()
+    .refine((phone) => /^\+\d{10,15}$/.test(phone), "Invalid Phone Number"),
+});
