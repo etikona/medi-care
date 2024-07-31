867bad73a0f3703b51d83b64c308ea6d6c71174e
eti kona <etikonapal@gmail.com>
2024-07-31 14:00:52 +0600

    get appointment action created

--- app/patients/[userId]/new-appointment/success/page.tsx
+++ app/patients/[userId]/new-appointment/success/page.tsx
@@ -1,15 +1,34 @@
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
-import fullLogo from "@/public/assets/icons/logo-full.svg"
+import fullLogo from "@/public/assets/icons/logo-full.svg";
+import success from "@/public/assets/gifs/success.gif";
 const Success = () => {
   return (
     <div className="flex h-screen max-h-screen px-[5%]">
       <div>
         <Link href="/">
-        <Image src={fullLogo} alt="" />
+          <Image
+            src={fullLogo}
+            alt="logo"
+            height={1000}
+            width={1000}
+            className="h-10 w-fit"
+          />
         </Link>
-        </div>
+        <section className="flex flex-col items-center">
+          <Image src={success} alt="success" height={300} width={280} />
+          <h2 className="header mb-6 max-w-[600px] text-center">
+            Your <span className="text-blue-700">appointment request</span> has
+            been successfully submitted.
+          </h2>
+          <p>We will touch shortly to confirm.</p>
+        </section>
+        <section className="request-details">
+          <p>Requested appointment details : </p>
+          <div className="flex items-center gap-3"></div>
+        </section>
+      </div>
     </div>
   );
 };
