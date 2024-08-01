3f9dce854bb107afd8255b47083862fd04b608c7
eti kona <etikonapal@gmail.com>
2024-08-01 08:56:13 +0600

    Patient side has been complete

--- app/patients/[userId]/new-appointment/success/page.tsx
+++ app/patients/[userId]/new-appointment/success/page.tsx
@@ -2,8 +2,22 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 import fullLogo from "@/public/assets/icons/logo-full.svg";
+import calender from "@/public/assets/icons/calendar.svg";
 import success from "@/public/assets/gifs/success.gif";
-const Success = () => {
+import { getAppointment } from "@/lib/actions/appointment.actions";
+import { Doctors } from "@/constants";
+import { formatDateTime } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+
+const Success = async ({
+  params: { userId },
+  searchParams,
+}: SearchParamProps) => {
+  const appointmentId = (searchParams?.appointmentId as string) || "";
+  const appointment = await getAppointment(appointmentId);
+  const doctor = Doctors.find(
+    (doc) => doc.name === appointment.primaryPhysician
+  );
   return (
     <div className="flex h-screen max-h-screen px-[5%]">
       <div>
@@ -26,8 +40,27 @@ const Success = () => {
         </section>
         <section className="request-details">
           <p>Requested appointment details : </p>
-          <div className="flex items-center gap-3"></div>
+          <div className="flex items-center gap-3">
+            <Image
+              src={doctor?.image!}
+              alt="doctor"
+              height={100}
+              width={100}
+              className="size-6"
+            />
+            <p className="whitespace-nowrap">{doctor?.name}</p>
+          </div>
+          <div className="flex gap-2">
+            <Image src={calender} alt="calender" height={24} width={24} />
+            <p>{formatDateTime(appointment.schedule).dateTime}</p>
+          </div>
         </section>
+        <Button variant="outline" className="shad-primary-btn" asChild>
+          <Link href={`/patients/${userId}/new-appointment`}>
+            New Appointment
+          </Link>
+        </Button>
+        <p className="copyright">© 2024 Medi-Care</p>
       </div>
     </div>
   );
