82100938e6573f496fa1f0e9a8fb63fb6d5a6a58
eti kona <etikonapal@gmail.com>
2024-08-02 10:31:04 +0600

    OTP checking

--- components/PassKeyModal.tsx
+++ components/PassKeyModal.tsx
@@ -16,16 +16,48 @@ import {
   InputOTPSeparator,
   InputOTPSlot,
 } from "@/components/ui/input-otp";
+import { decryptKey, encryptKey } from "@/lib/utils";
 
 import close from "@/public/assets/icons/close.svg";
 import Image from "next/image";
-import { useRouter } from "next/navigation";
-import { useState } from "react";
+import { usePathname, useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
 
 const PassKeyModal = () => {
   const router = useRouter();
+  const path = usePathname();
   const [open, setOpen] = useState(true);
   const [passkey, setPasskey] = useState("");
+  const [error, setError] = useState("");
+
+  const encryptedKey =
+    typeof window !== "undefined"
+      ? window.localStorage.getItem("accessKey")
+      : null;
+  useEffect(() => {
+    const accessKey = encryptedKey && decryptKey(encryptedKey);
+    if (path) {
+      if (accessKey === process.env.NEXT_PUBLIC_ADMIN_PASSKEY) {
+        setOpen(false);
+        router.push("/admin");
+      } else {
+        setError("Invalid OTP");
+      }
+    }
+  }, [encryptedKey]);
+  //* Validate OTP
+  const validatePasskey = (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => {
+    e.preventDefault();
+    if (passkey === process.env.NEXT_PUBLIC_ADMIN_PASSKEY) {
+      const encryptedKey = encryptKey(passkey);
+      localStorage.setItem("accessKey", encryptedKey);
+      setOpen(false);
+    } else {
+      setError("Invalid OTP");
+    }
+  };
   // * Close Modal Function
   const closeModal = () => {
     setOpen(false);
@@ -65,15 +97,20 @@ const PassKeyModal = () => {
               <InputOTPSlot className="shad-otp-slot" index={4} />
               <InputOTPSlot className="shad-otp-slot" index={5} />
             </InputOTPGroup>
-            {/* <InputOTPSeparator /> */}
-            {/* <InputOTPGroup>
-             
-            </InputOTPGroup> */}
           </InputOTP>
+          {error && (
+            <p className="shad-error text-14-regular mt-4 flex justify-center">
+              {error}
+            </p>
+          )}
         </div>
         <AlertDialogFooter>
-          <AlertDialogCancel>Cancel</AlertDialogCancel>
-          <AlertDialogAction>Continue</AlertDialogAction>
+          <AlertDialogAction
+            onClick={(e) => validatePasskey(e)}
+            className="shad-primary-btn w-full"
+          >
+            Enter the OTP
+          </AlertDialogAction>
         </AlertDialogFooter>
       </AlertDialogContent>
     </AlertDialog>
