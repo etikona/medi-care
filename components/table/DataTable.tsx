80f757993caa95510142b792c4f69a0b97abd401
eti kona <etikonapal@gmail.com>
2024-08-15 19:49:43 +0600

    Data pagination functionality added in table

--- components/table/DataTable.tsx
+++ components/table/DataTable.tsx
@@ -16,6 +16,7 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
+import { Button } from "../ui/button";
 
 interface DataTableProps<TData, TValue> {
   columns: ColumnDef<TData, TValue>[];
@@ -76,6 +77,24 @@ export function DataTable<TData, TValue>({
             </TableRow>
           )}
         </TableBody>
+        <div className="flex items-center justify-end space-x-2 py-4">
+          <Button
+            variant="outline"
+            size="sm"
+            onClick={() => table.previousPage()}
+            disabled={!table.getCanPreviousPage()}
+          >
+            Previous
+          </Button>
+          <Button
+            variant="outline"
+            size="sm"
+            onClick={() => table.nextPage()}
+            disabled={!table.getCanNextPage()}
+          >
+            Next
+          </Button>
+        </div>
       </Table>
     </div>
   );
